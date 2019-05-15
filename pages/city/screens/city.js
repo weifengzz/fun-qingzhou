4daf6be09ad9c9b00e08775b2632dffc94baa6f6
SongXiming <koohead@outlook.com>
2019-05-15 16:47:00 +0800

    init

---
+++ pages/city/screens/city.js
@@ -0,0 +1,353 @@
+// pages/city/screens/city.js
+
+/**
+ * 获取位置信息
+ */
+const getLocation = (that) => {
+  wx.getSetting({
+    success: (res) => {
+      console.log(JSON.stringify(res))
+      // res.authSetting['scope.userLocation'] == undefined    表示 初始化进入该页面
+      // res.authSetting['scope.userLocation'] == false    表示 非初始化进入该页面,且未授权
+      // res.authSetting['scope.userLocation'] == true    表示 地理位置授权
+      if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+        wx.showModal({
+          title: '请求授权当前位置',
+          content: '需要获取您的地理位置，请确认授权',
+          success: function(res) {
+            if (res.cancel) {
+              wx.showToast({
+                title: '拒绝授权',
+                icon: 'none',
+                duration: 1000
+              })
+            } else if (res.confirm) {
+              wx.openSetting({
+                success: function(dataAu) {
+                  if (dataAu.authSetting["scope.userLocation"] == true) {
+                    wx.showToast({
+                      title: '授权成功',
+                      icon: 'success',
+                      duration: 1000
+                    })
+                    //再次授权，调用wx.getLocation的API
+
+                  } else {
+                    wx.showToast({
+                      title: '授权失败',
+                      icon: 'none',
+                      duration: 1000
+                    })
+                  }
+                }
+              })
+            }
+          }
+        })
+      } else if (res.authSetting['scope.userLocation'] == undefined) {
+        // 获取中心坐标
+        wx.getLocation({
+          success: function(res) {
+            that.setData({
+              location: {
+                longitude: res.longitude,
+                latitude: res.latitude
+              }
+            })
+            console.log(res.longitude)
+            console.log(res.latitude)
+          }
+        })
+        //调用wx.getLocation的API
+      } else {
+        // 获取中心坐标
+        wx.getLocation({
+          success: function(res) {
+            that.setData({
+              location: {
+                longitude: res.longitude,
+                latitude: res.latitude
+              }
+            })
+            console.log(res.longitude)
+            console.log(res.latitude)
+          }
+        })
+      }
+    }
+  })
+}
+
+const IMG_WIDTH = 300
+const IMG_HEIGHT = 420
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    location: {
+      longitude: 118.47966,
+      latitude: 36.68456
+    },
+    markers: [{
+      iconPath: '/assets/icons/logo.png',
+      id: 0,
+      latitude: 36.68456,
+      longitude: 118.47966,
+      width: 30,
+      height: 30,
+      callout: {
+        content: ' 八喜旅游网 ',
+        bgColor: '#ffffff',
+        padding: 5,
+        borderRadius: 5,
+        display: 'ALWAYS'
+      },
+      // label: {
+      //   content: ' 八喜旅游网 ',
+      //   bgColor: '#ffffff',
+      //   padding: 5,
+      //   borderRadius: 5
+      // }
+    }, {
+        iconPath: '/assets/icons/logo.png',
+        id: 0,
+        latitude: 36.64456,
+        longitude: 118.77966,
+        width: 30,
+        height: 30,
+        callout: {
+          content: ' 八喜旅游网 ',
+          bgColor: '#ffffff',
+          padding: 5,
+          borderRadius: 5,
+          display: 'ALWAYS'
+        },
+        // label: {
+        //   content: ' 八喜旅游网 ',
+        //   bgColor: '#ffffff',
+        //   padding: 5,
+        //   borderRadius: 5
+        // }
+      }, {
+        iconPath: '/assets/icons/logo.png',
+        id: 0,
+        latitude: 36.68496,
+        longitude: 118.45966,
+        width: 30,
+        height: 30,
+        callout: {
+          content: ' 八喜旅游网 ',
+          bgColor: '#ffffff',
+          padding: 5,
+          borderRadius: 5,
+          display: 'ALWAYS'
+        },
+        // label: {
+        //   content: ' 八喜旅游网 ',
+        //   bgColor: '#ffffff',
+        //   padding: 5,
+        //   borderRadius: 5
+        // }
+      }, {
+        iconPath: '/assets/icons/logo.png',
+        id: 0,
+        latitude: 36.18456,
+        longitude: 118.43966,
+        width: 30,
+        height: 30,
+        callout: {
+          content: ' 八喜旅游网 ',
+          bgColor: '#ffffff',
+          padding: 5,
+          borderRadius: 5,
+          display: 'ALWAYS'
+        },
+        // label: {
+        //   content: ' 八喜旅游网 ',
+        //   bgColor: '#ffffff',
+        //   padding: 5,
+        //   borderRadius: 5
+        // }
+      }, {
+        iconPath: '/assets/icons/logo.png',
+        id: 0,
+        latitude: 36.38456,
+        longitude: 118.41966,
+        width: 30,
+        height: 30,
+        callout: {
+          content: ' 八喜旅游网 ',
+          bgColor: '#ffffff',
+          padding: 5,
+          borderRadius: 5,
+          display: 'ALWAYS'
+        },
+        // label: {
+        //   content: ' 八喜旅游网 ',
+        //   bgColor: '#ffffff',
+        //   padding: 5,
+        //   borderRadius: 5
+        // }
+      }, {
+        iconPath: '/assets/icons/logo.png',
+        id: 0,
+        latitude: 36.38456,
+        longitude: 118.43966,
+        width: 30,
+        height: 30,
+        callout: {
+          content: ' 八喜旅游网 ',
+          bgColor: '#ffffff',
+          padding: 5,
+          borderRadius: 5,
+          display: 'ALWAYS'
+        },
+        // label: {
+        //   content: ' 八喜旅游网 ',
+        //   bgColor: '#ffffff',
+        //   padding: 5,
+        //   borderRadius: 5
+        // }
+      }, {
+        iconPath: '/assets/icons/logo.png',
+        id: 0,
+        latitude: 36.18456,
+        longitude: 118.45966,
+        width: 30,
+        height: 30,
+        callout: {
+          content: ' 八喜旅游网 ',
+          bgColor: '#ffffff',
+          padding: 5,
+          borderRadius: 5,
+          display: 'ALWAYS'
+        },
+        // label: {
+        //   content: ' 八喜旅游网 ',
+        //   bgColor: '#ffffff',
+        //   padding: 5,
+        //   borderRadius: 5
+        // }
+      }, {
+        iconPath: '/assets/icons/logo.png',
+        id: 0,
+        latitude: 36.68156,
+        longitude: 118.41966,
+        width: 30,
+        height: 30,
+        callout: {
+          content: ' 八喜旅游网 ',
+          bgColor: '#ffffff',
+          padding: 5,
+          borderRadius: 5,
+          display: 'ALWAYS'
+        },
+        // label: {
+        //   content: ' 八喜旅游网 ',
+        //   bgColor: '#ffffff',
+        //   padding: 5,
+        //   borderRadius: 5
+        // }
+      }, {
+        iconPath: '/assets/icons/logo.png',
+        id: 0,
+        latitude: 36.62456,
+        longitude: 118.27966,
+        width: 30,
+        height: 30,
+        callout: {
+          content: ' 八喜旅游网 ',
+          bgColor: '#ffffff',
+          padding: 5,
+          borderRadius: 5,
+          display: 'ALWAYS'
+        },
+        // label: {
+        //   content: ' 八喜旅游网 ',
+        //   bgColor: '#ffffff',
+        //   padding: 5,
+        //   borderRadius: 5
+        // }
+      }],
+    scale: 18
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+    getLocation(this)
+    this.mapCtx = wx.createMapContext('city')
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+
+  /**
+   * 地图缩放变化
+   */
+  bindregionchange: function(e) {},
+
+  /**
+   * 地图marker点击
+   */
+  markertap: function (e) {
+    var that = this;
+    that.setData({
+      scale: 18,
+      location: {
+        longitude: that.data.markers[0].longitude,
+        latitude: that.data.markers[0].latitude
+      }
+    })
+    console.log(e)
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,70 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+/**
+ * 跳转到首页
+ */
+const navigateHome = () => {
+  wx.showToast({
+    title: '登录成功',
+    icon: 'success',
+    duration: 1000
+  })
+  setTimeout(() => {
+    // 跳转到首页界面
+    wx.switchTab({
+      url: '/pages/home/screens/home'
+    })
+  }, 1000)
+}
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+      navigateHome()
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+        // 跳转到首页界面
+        navigateHome()
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+          // 跳转到首页界面
+          navigateHome()
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+    // 跳转到首页界面
+    navigateHome()
+  }
+})
