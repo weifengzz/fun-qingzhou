f9270e259a3c9776169989644ea45754fe7d0906
SongXiming <koohead@outlook.com>
2019-05-16 11:53:19 +0800

    添加一张图片item

---
+++ pages/home/components/img_item/img_item.js
@@ -0,0 +1,26 @@
+// pages/home/components/img_item/img_item.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    data: {
+      type: Object,
+      value: {}
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
