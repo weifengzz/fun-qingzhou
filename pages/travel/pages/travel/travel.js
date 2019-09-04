0c5918ad42fd8292d11c0f960a9da4af354d0252
weifengzz <weifengzz@outlook.com>
2019-09-04 12:41:09 +0800

    add: travel list

--- pages/travel/pages/travel/travel.js
+++ pages/travel/pages/travel/travel.js
@@ -1,66 +1,53 @@
 // pages/travel/screens/travel.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    list: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    wx.request({
+      url: 'http://127.0.0.1:3000/travel',
+      data: {},
+      method: "GET",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: (res) => {
+        this.setData({
+          list: res.data
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom() {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onItemTap(e) {
+    const data = e.currentTarget.dataset.item
+    wx.navigateTo({
+      url: '../detail/detail?data=' + JSON.stringify(data),
+    })
   }
 })
